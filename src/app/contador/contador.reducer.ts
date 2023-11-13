86f98eaf6461b31353478f9d7fff6a14321d1518
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-13 16:45:16 -0500

    reducer usando createReducer sin switch

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,15 +1,15 @@
-import { Action } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 import { decrementar, incrementar } from './contador.actions';
+
 
 export const initialState = 10;
 
-export function contadorReducer(state = initialState, action: Action) {
-  switch (action.type) {
-    case incrementar.type:
-      return (state + 1);
-    case decrementar.type:
-      return (state - 1);
-    default:
-      return state;
-  }
+//usando create state
+const _contadorReducer = createReducer(initialState,
+  on(incrementar, state => state + 1),
+  on(decrementar, state => state - 1),
+
+  )
+export function contadorReducer(state: number  = initialState, action: Action){
+  return _contadorReducer(state,action)
 }
