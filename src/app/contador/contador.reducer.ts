58ad584589cde097364a83fe520670a0166e2dfe
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-13 17:11:32 -0500

    se organizan imports en el reducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,22 +1,16 @@
 import { Action, createReducer, on } from '@ngrx/store';
-import {
-  decrementar,
-  dividir,
-  incrementar,
-  multiplicar,
-  reset,
-} from './contador.actions';
+import * as fromContador  from './contador.actions';
 
 export const initialState = 10;
 
 //usando create state
 const _contadorReducer = createReducer(
   initialState,
-  on(incrementar, (state) => state + 1),
-  on(decrementar, (state) => state - 1),
-  on(multiplicar, (state, payload) => state * payload.numero),
-  on(dividir, (state, { numero }) => state / numero), //usando desestructuración
-  on(reset, () => initialState)
+  on(fromContador.incrementar, (state) => state + 1),
+  on(fromContador.decrementar, (state) => state - 1),
+  on(fromContador.multiplicar, (state, payload) => state * payload.numero),
+  on(fromContador.dividir, (state, { numero }) => state / numero), //usando desestructuración
+  on(fromContador.reset, () => initialState)
 );
 
 export function contadorReducer(state: number = initialState, action: Action) {
